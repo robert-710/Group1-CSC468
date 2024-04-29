3893088221d3c083aaea1093397ea34016d5be3c
Robert Silver <143738061+robert-710@users.noreply.github.com>
2024-04-28 21:05:59 -0400

    Update setup_registry.sh

--- registry/setup_registry.sh
+++ registry/setup_registry.sh
@@ -13,7 +13,7 @@ cp /local/repository/registry/san.cnf.template san.cnf
 
 ip_address=$(ip addr | grep eth0| awk -F ' ' '{print $2}' | awk -F '/' '{print $1'} | tail -n 1)
 
-sed -i "s/IPADDR/${ip_address}/g" san.cnf
+sed -i "s/IPADDR/${ip_address}:8443/g" san.cnf
 openssl req -new -x509 -nodes -sha1 -days 365 -key domain.key -out domain.crt -config san.cnf
 
 # create login/password in the shared /keys directory
@@ -22,8 +22,8 @@ cd /opt/keys/auth
 docker run --rm --entrypoint htpasswd registry:2.7.0 -Bbn admin registry > htpasswd
 
 # create a template subdirectory to be mounted to pods
-mkdir -p /opt/keys/certs.d/${ip_address}:443
-cp /opt/keys/certs/domain.crt /opt/keys/certs.d/${ip_address}:443/ca.crt
+mkdir -p /opt/keys/certs.d/${ip_address}:8443
+cp /opt/keys/certs/domain.crt /opt/keys/certs.d/${ip_address}:8443/ca.crt
 
 # launch registry
 cp -R /opt/keys/certs.d /etc/docker
